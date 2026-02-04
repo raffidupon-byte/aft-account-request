80ff87c9b47c460623e92c1633095589ce79eecb
raffidupon-byte <raffidupon@gmail.com>
2026-02-04 23:29:34 +0100

    final test

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -2,17 +2,17 @@ module "account_request_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "sam1983fr+fortest+4@gmail.com"
-    AccountName               = "sami_first_account2"
-    ManagedOrganizationalUnit = "SANDBOX" 
-    SSOUserEmail              = "sam1983fr+fortest+4@gmail.com"
+    AccountEmail              = "sam1983fr+fortest+5@gmail.com"
+    AccountName               = "sami_first_account3"
+    ManagedOrganizationalUnit = "Sandbox" 
+    SSOUserEmail              = "sam1983fr+fortest+5@gmail.com"
     SSOUserFirstName          = "sami"
     SSOUserLastName           = "abboud"
 
   }
 
   account_tags = {
-    "ABC:Owner"       = "sam1983fr+fortest+4@gmail.com"
+    "ABC:Owner"       = "sam1983fr+fortest+5@gmail.com"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
@@ -32,5 +32,5 @@ module "account_request_01" {
     custom2 = "b"
   }
 
-  account_customizations_name = "SANDBOX"
+  account_customizations_name = "Sandbox"
 }
